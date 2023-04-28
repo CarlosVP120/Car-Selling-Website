1be50f79e41fc4cb14696aff38e87d2a919c2a9c
diegopartida22 <A01641113@tec.mx>
2023-04-28 08:20:53 -0600

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,58 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="title">Car</div>;
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/inventario")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    return <div>Error al obtener los datos: {error.message}</div>;
+  }
+
+  return (
+    <div className="title">
+      <h1>Carros</h1>
+      <p>Lista de carros</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Año</th>
+            <th>Precio</th>
+            <th>Estado</th>
+            <th>Color</th>
+            <th>Imagen</th>
+            <th>Stock</th>
+            <th>URL Imagen</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.result &&
+            data.result.map((item) => (
+              <tr key={item.id}>
+                <td>{item.marca}</td>
+                <td>{item.modelo}</td>
+                <td>{item.año}</td>
+                <td>{item.precio}</td>
+                <td>{item.estado}</td>
+                <td>{item.color}</td>
+                <td>{item.imagen}</td>
+                <td>{item.stock}</td>
+                <td>{item.url_imagen}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
