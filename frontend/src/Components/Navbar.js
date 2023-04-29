4fcab527cd22bf7053476a7bb63e4cdb1f113a50
diegopartida22 <A01641113@tec.mx>
2023-04-29 00:27:13 -0600

    Add dropdown

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleDropdownClick = (option) => {
+    setSelectedOption(option);
+    setShowDropdown(false);
+  };
+
   return (
     <div className="navbar">
-      {/* Logo */}
       <img src="car-icon.png" alt="" className="logo-icon" />
-      {/* Links */}
       <div className="navbar-links">
         <a href="/">Inicio</a>
         <a href="/inventario">Inventario</a>
         <a href="/contacto">Contacto</a>
       </div>
-      {/* Seller Panel Button*/}
-      <button type="button" className="seller-button">
-        Ingresar
-      </button>
+
+      <div className="seller-dropdown">
+        <div className="dropdown-button">
+          {selectedOption && (
+            <p className="selected-option">{selectedOption}</p>
+          )}
+          <button
+            type="button"
+            className="seller-button"
+            onClick={() => setShowDropdown(!showDropdown)}
+          >
+            Ingresar
+          </button>
+        </div>
+        {showDropdown && (
+          <div className="dropdown-content">
+            <button onClick={() => handleDropdownClick("Comprador")}>
+              Comprador
+            </button>
+            <button onClick={() => handleDropdownClick("Vendedor")}>
+              Vendedor
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
