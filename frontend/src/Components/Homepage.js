536dfcdd0d02fc16237e3ee37249e097df2e53d7
CarlosVP120 <carlosveryan4@gmail.com>
2023-04-29 00:44:05 -0600

    Homepage

--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -5,7 +5,12 @@ function Homepage() {
   return (
     <div className="container">
       <h3 className="homepage-subtitle">Conoce tu nuevo auto</h3>
-      <h2 className="homepage-title">BMW M4 2022</h2>
+
+      <h2 className="homepage-title">
+        BMW
+        <img src="img/m4.png" alt="BMW M4 2022" className="m4-logo" />
+        2022
+      </h2>
       <div className="homepage-buttons">
         <button type="button" className="homepage-button">
           Ver más
